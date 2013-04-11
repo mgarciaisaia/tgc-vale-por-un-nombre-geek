f886dfb0d4a5c5e9e5f44bf807310b66c1378c28
dbkazarian@gmail.com <dbkazarian>
2013-04-11 20:18:58 +0000

    Comprobacion de indices en getHeight

--- MiGrupo/Pruebas/Terrain.cs
+++ MiGrupo/Pruebas/Terrain.cs
@@ -1,42 +1,49 @@
 ﻿using TgcViewer.Utils.Terrain;
 using Microsoft.DirectX;
+using TgcViewer.Utils.TgcGeometry;
+using System.Drawing;
+using Microsoft.DirectX.Direct3D;
+using TgcViewer;
+using System;
 
 namespace AlumnoEjemplos.MiGrupo.Pruebas
 {
-    class Terrain:TgcSimpleTerrain
+    class Terrain : TgcSimpleTerrain
     {
         float scaleXZ;
         float scaleY;
         float halfWidth;
         float halfLength;
 
+
+
         public new void loadHeightmap(string heightmapPath, float scaleXZ, float scaleY, Vector3 center)
         {
             this.scaleXZ = scaleXZ;
             this.scaleY = scaleY;
             base.loadHeightmap(heightmapPath, scaleXZ, scaleY, center);
-            halfWidth = (float)HeightmapData.GetLength(0)/2;
-            halfLength = (float)HeightmapData.GetLength(1)/2;
+            halfWidth = (float)HeightmapData.GetLength(0) / 2;
+            halfLength = (float)HeightmapData.GetLength(1) / 2;
+
 
         }
 
-      
+
         public int getHeight(float x, float z)
         {
             int height;
-          
-            int xInt = (int)x;
-            int zInt = (int)z;
-            xInt = xInt + (int)(scaleXZ * halfWidth); 
-            zInt = zInt + (int)(scaleXZ * halfLength);
+            int i, j;
+
+            i = (int)(x / scaleXZ + halfWidth);
+            j = (int)(z / scaleXZ + halfLength);
+
+            if (i >= HeightmapData.GetLength(0) || j >= HeightmapData.GetLength(1) || j < 0 || i < 0) return 0;
+
+            height = (int)(HeightmapData[i, j] * scaleY);
            
-            height = HeightmapData[(int)(xInt / scaleXZ), (int)(zInt / scaleXZ)];
-            height = (int)(height * scaleY);
             return height;
         }
 
-        
-    
 
     }
 }
