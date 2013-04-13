c9a6003279bc2d65360f149b2ea06c8a07856df2
dbkazarian@gmail.com <dbkazarian@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-04-13 16:46:26 +0000

    Eliminados los using que no se usan

--- trunk/MiGrupo/Pruebas/PruebaMoverPersonaje.cs
+++ trunk/MiGrupo/Pruebas/PruebaMoverPersonaje.cs
@@ -1,13 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 using TgcViewer.Example;
 using TgcViewer;
 using Microsoft.DirectX.Direct3D;
 using System.Drawing;
 using Microsoft.DirectX;
-using TgcViewer.Utils.Modifiers;
-using TgcViewer.Utils.TgcSkeletalAnimation;
 using TgcViewer.Utils.TgcGeometry;
 using TgcViewer.Utils.TgcSceneLoader;
 using TgcViewer.Utils.TgcKeyFrameLoader;
