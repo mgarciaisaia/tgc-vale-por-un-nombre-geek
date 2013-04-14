2752e3e81e811349356984cd444cf9f63db9d3ae
dbkazarian@gmail.com <dbkazarian@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-04-14 05:44:14 +0000

    PruebaVision.cs

---
+++ trunk/MiGrupo/Pruebas/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TgcViewer.Utils.TgcSkeletalAnimation;
+using Microsoft.DirectX.Direct3D;
+using TgcViewer;
+using TgcViewer.Utils.TgcSceneLoader;
+using TgcViewer.Utils.TgcGeometry;
+using Microsoft.DirectX;
+
+namespace AlumnoEjemplos.MiGrupo.ValePorUnNombreGeek.Enemy
+{
+   
+    public class Enemy : TgcSkeletalMesh
+    {
+        
+
+       
+        public Enemy(Mesh mesh, string name, MeshRenderType renderType, TgcSkeletalBone[] bones)
+            : base(mesh, name, renderType, bones)
+        {
+            GuiController.Instance.Modifiers.addFloat("RadioVision", 0, 1000, 500);
+            GuiController.Instance.Modifiers.addFloat("AnguloVision",0,360,90);
+        }
+
+        public bool puedeVer(TgcBox target)
+        {
+
+            float radioVision = (float)GuiController.Instance.Modifiers.getValue("RadioVision");
+            float anguloVision = (float)GuiController.Instance.Modifiers.getValue("AnguloVision");
+            
+         
+            return false;
+        }
+
+     
+
+    }
+
+    public class EnemyFactory : TgcSkeletalLoader.IMeshFactory
+    {
+        public TgcSkeletalMesh createNewMesh(Mesh d3dMesh, string meshName, TgcSkeletalMesh.MeshRenderType renderType, TgcSkeletalBone[] bones)
+        {
+            return new Enemy(d3dMesh, meshName, renderType, bones);
+        }
+    }
+
+}
---
+++ trunk/MiGrupo/Pruebas/PruebaVision.cs
@@ -0,0 +1,115 @@
+﻿using TgcViewer.Example;
+using TgcViewer;
+using TgcViewer.Utils.TgcGeometry;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using AlumnoEjemplos.MiGrupo.ValePorUnNombreGeek.Enemy;
+using TgcViewer.Utils.TgcSceneLoader;
+using TgcViewer.Utils.TgcSkeletalAnimation;
+using System;
+using System.Drawing;
+
+
+namespace AlumnoEjemplos.ValePorUnNombreGeek.PruebaVision
+{
+    /// <summary>
+    /// Prueba de vision
+    /// </summary>
+    public class PruebaVision : TgcExample
+    {
+     
+        public override string getCategory()
+        {
+            return "AlumnoEjemplos";
+        }
+
+        /// <summary>
+        /// Completar nombre del grupo en formato Grupo NN
+        /// </summary>
+        public override string getName()
+        {
+            return "Prueba de vision";
+        }
+
+        /// <summary>
+        /// Completar con la descripción del TP
+        /// </summary>
+        public override string getDescription()
+        {
+            return "Prueba de vision";
+        }
+
+        TgcBox piso, caja;
+        Enemy enemigo;
+
+        public override void init(){
+        
+
+            Microsoft.DirectX.Direct3D.Device d3dDevice = GuiController.Instance.D3dDevice;
+            String mediaDir = GuiController.Instance.AlumnoEjemplosMediaDir;
+            TgcTexture pisoTexture = TgcTexture.createTexture(d3dDevice, GuiController.Instance.ExamplesMediaDir + "Texturas\\tierra.jpg");
+            
+            piso = TgcBox.fromSize(new Vector3(0, 0, 0), new Vector3(1000, 5, 1000), pisoTexture);
+
+            caja = TgcBox.fromSize(new Vector3(0, 0, -10), new Vector3(10, 100, 10), Color.Blue);
+
+            //Crear loader
+            TgcSkeletalLoader loader = new TgcSkeletalLoader();
+
+            //Configurar MeshFactory customizado
+            loader.MeshFactory = new EnemyFactory();
+
+                   
+
+            enemigo = (Enemy)loader.loadMeshAndAnimationsFromFile(
+                mediaDir + "SkeletalAnimations\\BasicHuman\\" + "BasicHuman-TgcSkeletalMesh.xml",
+                new string[] { 
+                    mediaDir + "SkeletalAnimations\\BasicHuman\\Animations\\" + "Walk-TgcSkeletalAnim.xml",
+                    mediaDir + "SkeletalAnimations\\BasicHuman\\Animations\\" + "StandBy-TgcSkeletalAnim.xml",
+                      });
+
+
+            enemigo.playAnimation("StandBy");
+
+            enemigo.Position = new Vector3(0, 0, 0);
+
+            GuiController.Instance.RotCamera.targetObject(enemigo.BoundingBox);
+            GuiController.Instance.Modifiers.addVertex3f("posicionCaja", new Vector3(-1000, -1000, -1000), new Vector3(1000, 1000, 1000), new Vector3(0, 0, -20));
+
+                 
+
+        }
+
+
+
+      
+
+
+        public override void render(float elapsedTime)
+        {
+           piso.render();
+           enemigo.render();
+           caja.Position = (Vector3)GuiController.Instance.Modifiers.getValue("posicionCaja");
+           if (enemigo.puedeVer(caja)) caja.Color = Color.Green; else caja.Color = Color.Red;
+           caja.updateValues();
+           caja.render();
+
+        }
+
+
+        public override void close()
+        {
+            piso.dispose();
+            enemigo.dispose();
+        }
+
+
+    }
+
+   
+ 
+
+
+
+
+}
