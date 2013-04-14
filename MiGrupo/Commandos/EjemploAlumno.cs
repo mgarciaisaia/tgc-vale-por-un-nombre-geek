1bac312d2ffc8c576eb82266830d7bf70f325998
pablo.santiago.fernandez@gmail.com <pablo.santiago.fernandez@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-04-14 14:28:19 +0000

    Empezando a laburar en el Commmandos....

--- MiGrupo/Commandos/EjemploAlumno.cs
+++ MiGrupo/Commandos/EjemploAlumno.cs
@@ -23,19 +23,10 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek
     /// </summary>
     public class EjemploAlumno : TgcExample
     {
-        TgcBox piso;
-        List<TgcBox> obstaculos;
-
-
-
-        TgcScene escenario;
-        TgcSkeletalMesh personaje;
-        List<Collider> objetosColisionables = new List<Collider>();
-        TgcElipsoid characterElipsoid;
-        ElipsoidCollisionManager collisionManager;
-
         Ambiente ambiente;
         Camara camara;
+        Personaje personaje;
+        Terreno terreno;
 
         /// <summary>
         /// Categoría a la que pertenece el ejemplo.
@@ -74,61 +65,17 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek
             //Device de DirectX para crear primitivas
             Microsoft.DirectX.Direct3D.Device d3dDevice = GuiController.Instance.D3dDevice;
 
-            //Carpeta de archivos Media del alumno
-            string alumnoMediaFolder = GuiController.Instance.AlumnoEjemplosMediaDir;
-
-            TgcSceneLoader loader = new TgcSceneLoader();
-            escenario = loader.loadSceneFromFile(GuiController.Instance.ExamplesMediaDir + "\\MeshCreator\\Scenes\\Mountains\\Mountains-TgcScene.xml");
-
-            //Crear piso
-            TgcTexture pisoTexture = TgcTexture.createTexture(d3dDevice, GuiController.Instance.ExamplesMediaDir + "Texturas\\tierra.jpg");
-            piso = TgcBox.fromSize(new Vector3(0, -60, 0), new Vector3(1000, 5, 1000), pisoTexture);
-
-            objetosColisionables.Clear();
-            foreach (TgcMesh mesh in escenario.Meshes)
-            {
-                //Los objetos del layer "TriangleCollision" son colisiones a nivel de triangulo
-                if (mesh.Layer == "TriangleCollision")
-                {
-                    objetosColisionables.Add(TriangleMeshCollider.fromMesh(mesh));
-                }
-                //El resto de los objetos son colisiones de BoundingBox. Las colisiones a nivel de triangulo son muy costosas asi que deben utilizarse solo
-                //donde es extremadamente necesario (por ejemplo en el piso). El resto se simplifica con un BoundingBox
-                else
-                {
-                    objetosColisionables.Add(BoundingBoxCollider.fromBoundingBox(mesh.BoundingBox));
-                }
-            }
-
-            //Cargar personaje con animaciones
-            TgcSkeletalLoader skeletalLoader = new TgcSkeletalLoader();
-            personaje = skeletalLoader.loadMeshAndAnimationsFromFile(
-                GuiController.Instance.ExamplesMediaDir + "SkeletalAnimations\\BasicHuman\\" + "BasicHuman-TgcSkeletalMesh.xml",
-                new string[] { 
-                    GuiController.Instance.ExamplesMediaDir + "SkeletalAnimations\\BasicHuman\\Animations\\" + "Walk-TgcSkeletalAnim.xml",
-                    GuiController.Instance.ExamplesMediaDir + "SkeletalAnimations\\BasicHuman\\Animations\\" + "StandBy-TgcSkeletalAnim.xml",
-                    GuiController.Instance.ExamplesMediaDir + "SkeletalAnimations\\BasicHuman\\Animations\\" + "Jump-TgcSkeletalAnim.xml"
-                });
-            //Configurar animacion inicial
-            personaje.playAnimation("StandBy", true);
-            //Escalarlo porque es muy grande
-            personaje.Position = new Vector3(0, 1000, -150);
-            //Rotarlo 180° porque esta mirando para el otro lado
-            personaje.rotateY(Geometry.DegreeToRadian(180f));
-
-            //BoundingSphere que va a usar el personaje
-            personaje.AutoUpdateBoundingBox = false;
-            characterElipsoid = new TgcElipsoid(personaje.BoundingBox.calculateBoxCenter() + new Vector3(0, 0, 0), new Vector3(12, 28, 12));
-
-            //Crear manejador de colisiones
-            collisionManager = new ElipsoidCollisionManager();
-            collisionManager.GravityEnabled = true;
-
-
             //Crear SkyBox
             ambiente = new Ambiente();
+
+            //Cargar HeightMap
+            terreno = new Terreno();
+
+            //Crear personaje
+            personaje = new Personaje(terreno.getPosition(0, 0));
+
             //Inicializar camara
-            camara = new Camara(personaje.Position);
+            camara = new Camara(personaje.getPosition());
 
 
         }
@@ -144,110 +91,10 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek
         {
             Microsoft.DirectX.Direct3D.Device d3dDevice = GuiController.Instance.D3dDevice;
 
-            camara.refresh(400);
+            camara.refresh(500);
             ambiente.render();
-
-            //Calcular proxima posicion de personaje segun Input
-            float moveForward = 0f;
-            float rotate = 0;
-            bool moving = false;
-            bool rotating = false;
-            TgcD3dInput d3dInput = GuiController.Instance.D3dInput;
-
-            //Adelante
-            if (d3dInput.keyDown(Key.W))
-            {
-                moveForward = -1;
-                moving = true;
-            }
-
-            //Atras
-            if (d3dInput.keyDown(Key.S))
-            {
-                moveForward = 1;
-                moving = true;
-            }
-
-            //Derecha
-            if (d3dInput.keyDown(Key.D))
-            {
-                rotate = 150f;
-                rotating = true;
-            }
-
-            //Izquierda
-            if (d3dInput.keyDown(Key.A))
-            {
-                rotate = -150f;
-                rotating = true;
-            }
-
-            //Si hubo rotacion
-            if (rotating)
-            {
-                //Rotar personaje y la camara, hay que multiplicarlo por el tiempo transcurrido para no atarse a la velocidad el hardware
-                float rotAngle = Geometry.DegreeToRadian(rotate * elapsedTime);
-                personaje.rotateY(rotAngle);
-                //GuiController.Instance.ThirdPersonCamera.rotateY(rotAngle);
-            }
-
-            //Moviendose
-            if (moving)
-            {
-                //Activar animacion de caminando
-                personaje.playAnimation("Walk", true);
-            }
-            //Si no se esta moviendo ni saltando, activar animacion de Parado
-            else
-            {
-                personaje.playAnimation("StandBy", true);
-            }
-
-
-            //Vector de movimiento
-            Vector3 movementVector = Vector3.Empty;
-            if (moving)
-            {
-                //Aplicar movimiento, desplazarse en base a la rotacion actual del personaje
-                movementVector = new Vector3(
-                    FastMath.Sin(personaje.Rotation.Y) * moveForward,
-                    0,
-                    FastMath.Cos(personaje.Rotation.Y) * moveForward
-                    );
-            }
-
-
-            //Actualizar valores de gravedad
-            collisionManager.GravityEnabled = true;
-            collisionManager.GravityForce = new Vector3(0, -4, 0);
-            collisionManager.SlideFactor = 1f;
-            collisionManager.OnGroundMinDotValue = 0.72f;
-
-            //Mover personaje con detección de colisiones, sliding y gravedad
-            //Aca se aplica toda la lógica de detección de colisiones del CollisionManager. Intenta mover el Elipsoide
-            //del personaje a la posición deseada. Retorna la verdadera posicion (realMovement) a la que se pudo mover
-            Vector3 realMovement = collisionManager.moveCharacter(characterElipsoid, movementVector, objetosColisionables);
-            personaje.move(realMovement);
-
-
-            //GuiController.Instance.ThirdPersonCamera.setCamera(personaje.Position, 300, -300);
-
-            //Cargar desplazamiento realizar en UserVar
-            //GuiController.Instance.UserVars.setValue("Movement", TgcParserUtils.printVector3(realMovement));
-
-
-            //Render de mallas
-            foreach (TgcMesh mesh in escenario.Meshes)
-            {
-                mesh.render();
-            }
-
-
-            //Render personaje
-            personaje.animateAndRender();
-            characterElipsoid.render();
-
-
+            terreno.render();
+            personaje.render(elapsedTime);
         }
 
         /// <summary>
@@ -256,10 +103,9 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek
         /// </summary>
         public override void close()
         {
-            escenario.disposeAll();
-            personaje.dispose();
             ambiente.dispose();
-            characterElipsoid.dispose();
+            terreno.dispose();
+            personaje.dispose();
         }
 
     }
