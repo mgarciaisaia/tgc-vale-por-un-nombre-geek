d45d56e5d2bb0f4d3d0ff01a42165c062ecfb401
pablo.santiago.fernandez@gmail.com <pablo.santiago.fernandez@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-04-16 22:07:05 +0000

    Cambio en el metodo getRayIntersection que gana 20% mas fps (al menos en mi pc) cuando seleccionamos personajes. Antes la interseccion se buscaba desde el cielo hacia el mapa.. ahora arrancamos desde el punto mas bajo del heightmap (y=0) hasta cruzarnos con el mapa. En resumen, salvamos muchos ciclos de un horrible while.

--- ValePorUnNombreGeek/src/commandos/picking/PickingRayHome.cs
+++ ValePorUnNombreGeek/src/commandos/picking/PickingRayHome.cs
@@ -35,6 +35,41 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.picking
 
         public Vector3 getRayIntersection(Terrain terrain)
         {
+            return this.getRayIntersectionFromEarthToHeaven(terrain);
+        }
+
+        private Vector3 getRayIntersectionFromEarthToHeaven(Terrain terrain)
+        {
+            //Version que va "de la tierra al cielo" -> beneficia ENORMEMENTE picking en terrenos bajos
+            Vector3 aPoint;
+            float TERRAIN_MIN_Y = terrain.Position.Y;
+            float i0 = (TERRAIN_MIN_Y - this.getRay().Origin.Y) / this.getRay().Direction.Y;
+            float i = i0;
+
+            while (true)
+            {
+                aPoint = this.getRay().Origin + i * this.getRay().Direction;
+
+                if (GeneralMethods.isCloseTo(aPoint.Y, terrain.getHeight(aPoint.X, aPoint.Z)))
+                {
+                    //encontramos el punto de interseccion
+                    return aPoint;
+                }
+
+                if (aPoint.Y >= 255 * terrain.getScaleY())
+                {
+                    //ya nos estamos llendo al cielo...
+                    return this.getRay().Origin + i0 * this.getRay().Direction;
+                }
+
+                i--;
+            }
+        }
+
+        private Vector3 getRayIntersectionFromHeavenToEarth(Terrain terrain)
+        {
+            //Version que va "del cielo a la tierra" -> beneficia en gran medida picking en terrenos altos
+            //actualmente fuera de uso.
             Vector3 aPoint;
             float i = 0;
 
