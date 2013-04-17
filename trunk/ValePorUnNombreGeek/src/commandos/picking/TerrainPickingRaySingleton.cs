3a2277785a3726266215e15a514e0d335d356903
dbkazarian@gmail.com <dbkazarian@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-04-17 18:39:28 +0000

    Faltaba

---
+++ trunk/ValePorUnNombreGeek/src/commandos/picking/TerrainPickingRaySingleton.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TgcViewer.Utils.TgcGeometry;
+using Microsoft.DirectX;
+using TgcViewer;
+
+namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.picking
+{
+    class TerrainPickingRaySingleton : TgcPickingRay
+    {
+
+        private static TerrainPickingRaySingleton instance;
+
+
+        public static TerrainPickingRaySingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new TerrainPickingRaySingleton();
+                    
+                    GuiController.Instance.UserVars.addVar("HeightmapCoords");
+                    GuiController.Instance.UserVars.addVar("WorldCoords");
+                }
+                return instance;
+            }
+
+        }
+
+
+
+
+        public bool terrainIntersection(Terrain terrain, out Vector3 position)
+        {
+            //Version que va "de la tierra al cielo" -> beneficia ENORMEMENTE picking en terrenos bajos
+            instance.updateRay();
+
+            Vector3 myPoint;
+            float terrainY;
+            float i0 = (terrain.Position.Y - instance.Ray.Origin.Y) / instance.Ray.Direction.Y;
+            float i = i0;
+            Vector2 coords;
+            while (true)
+            {
+                myPoint = this.Ray.Origin + i * this.Ray.Direction;
+
+                if (terrain.xzToHeightmapCoords(myPoint.X, myPoint.Z, out coords))
+                {
+                    terrainY = terrain.HeightmapData[(int)coords.X, (int) coords.Y] * terrain.getScaleY();
+                   
+                    if (GeneralMethods.isCloseTo(myPoint.Y, terrainY))
+                    {
+                        //encontramos el punto de interseccion
+                        position = myPoint;
+
+                        GuiController.Instance.UserVars.setValue("HeightmapCoords", coords);
+                        GuiController.Instance.UserVars.setValue("WorldCoords", position);
+
+                        return true;
+                    }
+                }
+                else if (myPoint.Y >= 255 * terrain.getScaleY())
+                {
+                    //ya nos estamos llendo al cielo...
+                    position = Vector3.Empty;
+                    return false;
+                }
+
+                i--;
+            }
+        }
+
+        public Vector3 getRayGroundIntersection(Terrain terrain)
+        {
+            //retorna el punto de colision con el plano y=0
+            //(pablo) lo uso para ver si el rayo vario su posicion. es mucho mas rapido que getRayIntersection; salva fps.
+            this.updateRay();
+
+            float t0 = (terrain.Position.Y - this.Ray.Origin.Y) / this.Ray.Direction.Y;
+            return this.Ray.Origin + t0 * this.Ray.Direction;
+        }
+
+    }
+  
+}
