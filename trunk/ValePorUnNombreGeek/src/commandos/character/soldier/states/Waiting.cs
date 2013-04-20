fe858630441bd2053629a0dc31da5b84c19e5639
pablo.santiago.fernandez@gmail.com <pablo.santiago.fernandez@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-04-20 19:49:59 +0000

    Primer intento de soldado aburrido que mira aleatoriamente sus alrededores en cada waitpoint. (Posible bug cuando se encuentra en pendientes: se da media vuelta bruscamente)

--- trunk/ValePorUnNombreGeek/src/commandos/character/soldier/states/Waiting.cs
+++ trunk/ValePorUnNombreGeek/src/commandos/character/soldier/states/Waiting.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.DirectX;
 
 namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.character.soldier.states
 {
@@ -10,22 +11,56 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.character.soldier.sta
         private float waitingTime;
         private float maxWaitingTime;
 
+        private float boringSeenAngle;
+        private float boringSeenAngleLimit;
+        private float boringSeenAngleDelta;
+
+        private Vector3 boringWatching;
+        private Random rnd = new Random();
+
         public Waiting(Soldier _soldier)
             : base(_soldier)
         {
             this.waitingTime = 0;
             this.maxWaitingTime = 5;
+
+
+            Vector3 facing = this.soldier.Representation.Facing;
+            this.boringSeenAngle = (float)Math.Acos(facing.X);
+
+            this.boringSeenAngleLimit = this.boringSeenAngle + 2 * (float)rnd.NextDouble();
+            this.boringSeenAngleDelta = 0.5f;
         }
 
         public override void update(float elapsedTime)
         {
             //Si espero suficiente tiempo, fijar el proximo waitpoint como objetivo
             this.waitingTime += elapsedTime;
+
             if (this.waitingTime > this.maxWaitingTime)
             {
                 this.soldier.setNextPositionTarget();
                 this.soldier.setState(new Walking(this.soldier));
+                return;
             }
+
+            this.boringSeenAngle += this.boringSeenAngleDelta * elapsedTime;
+
+            this.boringWatching.X = (float)Math.Cos(this.boringSeenAngle);
+            this.boringWatching.Z = (float)Math.Sin(this.boringSeenAngle);
+            this.soldier.Representation.faceTo(this.boringWatching);
+
+            if (this.boringSeenAngleDelta > 0 && (this.boringSeenAngle > this.boringSeenAngleLimit))
+            {
+                this.boringSeenAngleDelta = -this.boringSeenAngleDelta;
+                this.boringSeenAngleLimit = -2 * (float) rnd.NextDouble();
+            }
+            if (this.boringSeenAngleDelta < 0 && (this.boringSeenAngle < this.boringSeenAngleLimit))
+            {
+                this.boringSeenAngleDelta = -this.boringSeenAngleDelta;
+                this.boringSeenAngleLimit = 2 * (float)rnd.NextDouble();
+            }
+
         }
     }
 }
