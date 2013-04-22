bb54dbfc66b52c38a70070bf38b8c0f0544c0e39
pablo.santiago.fernandez@gmail.com <pablo.santiago.fernandez@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-04-22 17:31:19 +0000

    Rotacion de enemigos en waitpoints funcionando, aunque la IA se puede pulir mucho mas.

--- trunk/ValePorUnNombreGeek/src/commandos/character/soldier/states/Waiting.cs
+++ trunk/ValePorUnNombreGeek/src/commandos/character/soldier/states/Waiting.cs
@@ -10,35 +10,39 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.character.soldier.sta
     class Waiting : SoldierState
     {
         private float waitingTime;
-        private const float MAX_WAITING_TIME=10;
+        private const float MAX_WAITING_TIME = 30;
 
         private Random rnd = new Random();
 
+        private float haciaDondeEstoyViendo;
         private float haciaDondeQuieroVer;
+        private float velocidadDeRotacion;
         private float tiempoMirando;
         private float tiempoEnQueMeCanso;
+        private const float ROTATION_SPEED = 2;
 
 
         public Waiting(Soldier _soldier)
             : base(_soldier)
         {
             this.waitingTime = 0;
+            this.haciaDondeEstoyViendo = this.actualFacingAngle;
             this.haciaDondeQuieroVer = this.haciaDondeEstoyViendo;
             this.tiempoMirando = 0;
-            this.tiempoEnQueMeCanso = 2;
+            this.tiempoEnQueMeCanso = 0;
         }
 
         public override void update(float elapsedTime)
         {
             if (this.waitingTime > MAX_WAITING_TIME)
             {
-                if (this.haciaDondeQuieroVer != this.haciaDondeEstaLaProximaPosicion)
+                if (this.haciaDondeQuieroVer != this.nextPositionFacingAngle)
                 {
-                    this.haciaDondeQuieroVer = this.haciaDondeEstaLaProximaPosicion;
+                    this.haciaDondeQuieroVer = this.nextPositionFacingAngle;
                 }
                 else
                 {
-                    if (GeneralMethods.isCloseTo(this.haciaDondeEstoyViendo, this.haciaDondeQuieroVer, 2 * elapsedTime))
+                    if (GeneralMethods.isCloseTo(this.haciaDondeEstoyViendo, this.haciaDondeQuieroVer, ROTATION_SPEED * elapsedTime))
                     {
                         this.soldier.setNextPositionTarget();
                         this.soldier.setState(new Walking(this.soldier));
@@ -56,14 +60,25 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.character.soldier.sta
             this.tiempoMirando += elapsedTime;
             if (this.tiempoMirando > this.tiempoEnQueMeCanso)
             {
-                this.haciaDondeQuieroVer = 2 * (float)rnd.NextDouble();
+                float newAngle = (float)(this.haciaDondeEstoyViendo - 0.5 + rnd.NextDouble());
+                if (newAngle < 0) newAngle += 2;
+                if (newAngle >= 2) newAngle -= 2;
+
+                this.haciaDondeQuieroVer = newAngle;
+
+                if (2 - this.haciaDondeQuieroVer + this.haciaDondeEstoyViendo < this.haciaDondeQuieroVer - this.haciaDondeEstoyViendo)
+                    this.velocidadDeRotacion = -ROTATION_SPEED;
+                else this.velocidadDeRotacion = ROTATION_SPEED;
+
                 this.tiempoMirando = 0;
+                this.tiempoEnQueMeCanso = (float)(2 + 5 * rnd.NextDouble());
             }
             else
             {
                 if (this.haciaDondeEstoyViendo != this.haciaDondeQuieroVer)
                 {
-                    this.haciaDondeEstoyViendo += (this.haciaDondeQuieroVer - this.haciaDondeEstoyViendo) * 2 * elapsedTime;
+                    this.haciaDondeEstoyViendo += (this.haciaDondeQuieroVer - this.haciaDondeEstoyViendo) * this.velocidadDeRotacion * elapsedTime;
+                    this.actualFacingAngle = this.haciaDondeEstoyViendo;
                 }
                 else
                 {
@@ -73,26 +88,25 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.character.soldier.sta
         }
 
 
+        #region FacingFunctions
 
-
-
-        private float haciaDondeEstoyViendo
+        private float actualFacingAngle
         {
             get
             {
-                return (float)Math.Acos(this.soldier.Representation.Facing.X);
+                return this.angle(this.soldier.Representation.Facing.X, this.soldier.Representation.Facing.Z);
             }
             set
             {
                 Vector3 direction = new Vector3();
-                direction.X = (float)Math.Cos(value);
+                direction.X = this.cos(value);
                 direction.Y = 0;
-                direction.Z = (float)Math.Sin(value);
+                direction.Z = this.sin(value);
                 this.soldier.Representation.faceTo(direction);
             }
         }
 
-        private float haciaDondeEstaLaProximaPosicion
+        private float nextPositionFacingAngle
         {
             get
             {
@@ -100,23 +114,48 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.character.soldier.sta
                 direction.Y = 0;
                 direction.Normalize();
 
-                return (float)Math.Acos(direction.X);
+                return this.angle(direction.X, direction.Z);
             }
         }
 
-        /*
-        private float cos(float value)
+        #endregion
+
+
+        #region AngleFunctions
+
+        //Funciones auxiliares para trabajar con angulos.
+        private float cos(float angle)
         {
-            float ret = (float)Math.Cos(value);
-            //if (value > 0.5f || value < 1.5) ret = -ret;
-            return ret;
+            return (float)Math.Cos(angle * Math.PI);
         }
-        private float sin(float value)
+        private float sin(float angle)
         {
-            float ret = (float)Math.Sin(value);
-            //if (value > 1) ret = -ret;
-            return ret;
+            return (float)Math.Sin(angle * Math.PI);
         }
-         */
+        private float angle(float x, float y)
+        {
+            /* Antes que nada, si, esta funcion es espantosa.
+             * Poco performante, poco intuitiva, muy pero muy fea.
+             * Lo unico bueno es que se la llama una sola vez por waitpoint.
+             * 
+             * Si alguno tiene una mejor idea de como resolver este problema
+             * por favor reescriba toda la funcion. A mi no me da para mas
+             * la cabeza.
+             */
+
+            double acosX = Math.Acos(x) / Math.PI;
+            double asinY = Math.Asin(y) / Math.PI;
+            acosX = Math.Round(acosX, 1);
+            asinY = Math.Round(asinY, 1);
+
+            List<double> xResults = new List<double> { acosX, 2 - acosX };
+            List<double> yResults = new List<double> { asinY, 1 - asinY };
+
+            var results = xResults.Intersect(yResults);
+
+            return (float)results.First();
+        }
+
+        #endregion
     }
 }
