58703bf04254c6b26a715c54303d209f7d8ce49d
pablo.santiago.fernandez@gmail.com <pablo.santiago.fernandez@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-05-05 15:03:23 +0000

    Pude dibujar un cilindro :D lo que no es menor para mi jaja. Hay que mejorar un poco la performance. Hago muchos calculos por el tema de los bordes laterales. Y el cilindro todavia no se puede rotar.

--- trunk/ValePorUnNombreGeek/src/pruebas/cilindro/Cylinder.cs
+++ trunk/ValePorUnNombreGeek/src/pruebas/cilindro/Cylinder.cs
@@ -17,8 +17,9 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.pruebas.cilindro
         private float halfLength;
         private int renderColor;
 
-        private const int END_CAPS_RESOLUTION = 10;
+        private const int END_CAPS_RESOLUTION = 50;
         private CustomVertex.PositionColored[] vertices;
+        private CustomVertex.PositionColored[] bordes;
 
         public Cylinder(Vector3 _center, float _halfLength, float _radius)
         {
@@ -34,26 +35,56 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.pruebas.cilindro
             if (vertices == null)
             {
                 int verticesCount = (END_CAPS_RESOLUTION * 2 + 2) * 3;
+                verticesCount = verticesCount * 2; //por las dos tapas
                 this.vertices = new CustomVertex.PositionColored[verticesCount];
+                this.bordes = new CustomVertex.PositionColored[4]; //bordes laterales
             }
-
-            int index = 0;
 
             float step = FastMath.TWO_PI / (float)END_CAPS_RESOLUTION;
+            int index = 0;
 
-            // Plano XZ
             for (float a = 0f; a <= FastMath.TWO_PI; a += step)
             {
-                vertices[index++] = new CustomVertex.PositionColored(new Vector3(FastMath.Cos(a) * this.radius, this.halfLength, FastMath.Sin(a) * this.radius) + this.center, renderColor);
-                vertices[index++] = new CustomVertex.PositionColored(new Vector3(FastMath.Cos(a + step) * this.radius, -this.halfLength, FastMath.Sin(a + step) * this.radius) + this.center, renderColor);
+                //tapa superior
+                vertices[index++] = new CustomVertex.PositionColored(new Vector3(FastMath.Cos(a) * this.radius, this.halfLength, FastMath.Sin(a) * this.radius) + this.center, this.renderColor);
+                vertices[index++] = new CustomVertex.PositionColored(new Vector3(FastMath.Cos(a + step) * this.radius, this.halfLength, FastMath.Sin(a + step) * this.radius) + this.center, this.renderColor);
+
+                //tapa inferior
+                vertices[index++] = new CustomVertex.PositionColored(new Vector3(FastMath.Cos(a) * this.radius, -this.halfLength, FastMath.Sin(a) * this.radius) + this.center, this.renderColor);
+                vertices[index++] = new CustomVertex.PositionColored(new Vector3(FastMath.Cos(a + step) * this.radius, -this.halfLength, FastMath.Sin(a + step) * this.radius) + this.center, this.renderColor);
             }
+
+            this.updateBorders();
+        }
+
+        private void updateBorders()
+        {
+            Vector3 cameraPos = GuiController.Instance.CurrentCamera.getPosition();
+            Vector3 cameraSeen = cameraPos - this.center;
+            Vector3 vup = new Vector3(0, this.halfLength, 0);
+            Vector3 transversalALaCamara = Vector3.Cross(cameraSeen, vup);
+            transversalALaCamara.Normalize();
+            transversalALaCamara *= this.radius;
+
+            Vector3 puntoA = this.center + vup + transversalALaCamara;
+            Vector3 puntoB = this.center - vup + transversalALaCamara;
+
+            bordes[0] = new CustomVertex.PositionColored(puntoA, this.renderColor);
+            bordes[1] = new CustomVertex.PositionColored(puntoB, this.renderColor);
+
+            puntoA = this.center + vup - transversalALaCamara;
+            puntoB = this.center - vup - transversalALaCamara;
+
+            bordes[2] = new CustomVertex.PositionColored(puntoA, this.renderColor);
+            bordes[3] = new CustomVertex.PositionColored(puntoB, this.renderColor);
         }
 
         public void render()
         {
             Device d3dDevice = GuiController.Instance.D3dDevice;
-            //d3dDevice.VertexDeclaration = GuiController.Instance.Shaders.VdecPositionColored;
+            this.updateBorders();
             d3dDevice.DrawUserPrimitives(PrimitiveType.LineList, vertices.Length / 2, vertices);
+            d3dDevice.DrawUserPrimitives(PrimitiveType.LineList, bordes.Length / 2, bordes);
         }
 
         public void dispose()
