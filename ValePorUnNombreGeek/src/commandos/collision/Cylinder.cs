f764a51f24d465d4fc615a3c20c06f75fc338ddd
pablo.santiago.fernandez@gmail.com <pablo.santiago.fernandez@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-05-09 21:58:50 +0000

    Implemento la version emprolijada del cilindro :p

--- ValePorUnNombreGeek/src/commandos/collision/Cylinder.cs
+++ ValePorUnNombreGeek/src/commandos/collision/Cylinder.cs
@@ -15,22 +15,25 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.collision
         private Vector3 center;
         private float radius;
         private Vector3 halfHeight;
-        private int renderColor;
+        private int borderColor;
 
         private const int END_CAPS_RESOLUTION = 30; //cantidad de lineas por cada tapa
         private CustomVertex.PositionColored[] endCapsVertex; //vertices de las tapas
         private CustomVertex.PositionColored[] bordersVertex; //vertices de los bordes
-        
-
-        //TODO emprolijar toda la clase! la hice "a las chapas"
 
 
         public Cylinder(Vector3 _base, float _height, float _radius)
+            : this(_base, _height, _radius, Color.Yellow)
+        {
+            //nothing to do
+        }
+
+        public Cylinder(Vector3 _base, float _height, float _radius, Color _color)
         {
             this.radius = _radius;
             this.halfHeight = new Vector3(0, _height / 2, 0);
             this.center = _base + this.halfHeight;
-            this.renderColor = Color.Yellow.ToArgb();
+            this.borderColor = _color.ToArgb();
             this.updateDraw();
         }
 
@@ -58,9 +61,10 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.collision
 
         #region Draw
 
-        public void setColor(Color _color)
+        public Color Color
         {
-            this.renderColor = _color.ToArgb();
+            get { return Color.FromArgb(this.borderColor); }
+            set { this.borderColor = value.ToArgb(); this.updateDraw(); }
         }
 
         private void updateDraw()
@@ -81,22 +85,14 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.collision
                 Vector3 tapaPuntoA = new Vector3(FastMath.Cos(a) * this.radius, 0, FastMath.Sin(a) * this.radius);
                 Vector3 tapaPuntoB = new Vector3(FastMath.Cos(a + step) * this.radius, 0, FastMath.Sin(a + step) * this.radius);
                 //tapa superior
-                endCapsVertex[index++] = new CustomVertex.PositionColored(tapaPuntoA + this.halfHeight + this.center, this.renderColor);
-                endCapsVertex[index++] = new CustomVertex.PositionColored(tapaPuntoB + this.halfHeight + this.center, this.renderColor);
+                endCapsVertex[index++] = new CustomVertex.PositionColored(tapaPuntoA + this.halfHeight + this.center, this.borderColor);
+                endCapsVertex[index++] = new CustomVertex.PositionColored(tapaPuntoB + this.halfHeight + this.center, this.borderColor);
                 //tapa inferior
-                endCapsVertex[index++] = new CustomVertex.PositionColored(tapaPuntoA - this.halfHeight + this.center, this.renderColor);
-                endCapsVertex[index++] = new CustomVertex.PositionColored(tapaPuntoB - this.halfHeight + this.center, this.renderColor);
+                endCapsVertex[index++] = new CustomVertex.PositionColored(tapaPuntoA - this.halfHeight + this.center, this.borderColor);
+                endCapsVertex[index++] = new CustomVertex.PositionColored(tapaPuntoB - this.halfHeight + this.center, this.borderColor);
             }
-
+            //bordes laterales
             this.updateBordersDraw();
-        }
-
-        private void updateDrawColor()
-        {
-            for (int i = 0; i < this.endCapsVertex.Count(); i++)
-            {
-                this.endCapsVertex[i].Color = this.renderColor;
-            }
         }
 
         private void updateBordersDraw()
@@ -109,21 +105,20 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.collision
             Vector3 puntoA = this.center + this.halfHeight + transversalALaCamara;
             Vector3 puntoB = this.center - this.halfHeight + transversalALaCamara;
 
-            bordersVertex[0] = new CustomVertex.PositionColored(puntoA, this.renderColor);
-            bordersVertex[1] = new CustomVertex.PositionColored(puntoB, this.renderColor);
+            bordersVertex[0] = new CustomVertex.PositionColored(puntoA, this.borderColor);
+            bordersVertex[1] = new CustomVertex.PositionColored(puntoB, this.borderColor);
 
             puntoA = this.center + this.halfHeight - transversalALaCamara;
             puntoB = this.center - this.halfHeight - transversalALaCamara;
 
-            bordersVertex[2] = new CustomVertex.PositionColored(puntoA, this.renderColor);
-            bordersVertex[3] = new CustomVertex.PositionColored(puntoB, this.renderColor);
+            bordersVertex[2] = new CustomVertex.PositionColored(puntoA, this.borderColor);
+            bordersVertex[3] = new CustomVertex.PositionColored(puntoB, this.borderColor);
         }
 
         public void render()
         {
             Device d3dDevice = GuiController.Instance.D3dDevice;
             this.updateBordersDraw();
-            this.updateDrawColor();
             d3dDevice.DrawUserPrimitives(PrimitiveType.LineList, endCapsVertex.Length / 2, endCapsVertex);
             d3dDevice.DrawUserPrimitives(PrimitiveType.LineList, bordersVertex.Length / 2, bordersVertex);
         }
@@ -136,15 +131,12 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.collision
 
         #endregion
 
-        public bool thereIsCollision(Cylinder collider, out Vector3 n)
+        public bool thereIsCollisionCyCy(Cylinder collider, out Vector3 n)
         {
-            //nota: no se checkea la altura, debido a la naturaleza del juego
-            Vector3 distance = collider.Center - this.Center;
-            distance.Y = 0;
-            if (distance.Length() <= this.radius + collider.radius)
+            Vector3 distance = collider.Position - this.Position;
+            if (FastMath.Pow2(distance.X) + FastMath.Pow2(distance.Z) <= FastMath.Pow2(this.radius + collider.radius))
             {
-                distance = this.Center - collider.Center;
-                n = Vector3.Cross(this.halfHeight, distance);
+                n = Vector3.Cross(distance, this.halfHeight);
                 n = Vector3.Cross(n, this.halfHeight);
                 n.Normalize();
                 return true;
@@ -156,56 +148,48 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.collision
             }
         }
 
-        public bool thereIsCollision(TgcBoundingBox aabb, out Vector3 n)
+        public bool thereIsCollisionCyBB(TgcBoundingBox aabb, out Vector3 n)
         {
-            //nota: no se checkea la altura, debido a la naturaleza del juego
-            if (funesMori(aabb))
-            {
-                Vector3 boxDimensions = aabb.calculateSize() * 0.5f;
-                Vector2 boxDimensions2d = new Vector2(boxDimensions.X, boxDimensions.Z);
+            n = Vector3.Empty;
 
-                Vector3 boxCenter = aabb.Position + boxDimensions;
-                Vector3 centerToCenter = new Vector3(this.Center.X - boxCenter.X, 0, this.Center.Z - boxCenter.Z);
-                Vector2 centerToCenter2d = new Vector2(FastMath.Abs(centerToCenter.X), FastMath.Abs(centerToCenter.Z));
-
-                float cross = Vector2.Ccw(centerToCenter2d, boxDimensions2d);
-
-                if (cross > 0) n = new Vector3(centerToCenter.X, 0, 0);
-                else if (cross < 0) n = new Vector3(0, 0, centerToCenter.Z);
-                else
-                {
-                    n = new Vector3(centerToCenter.X * boxDimensions2d.Y, 0, centerToCenter.Z * boxDimensions2d.X);
-                }
-                n.Normalize();
-                return true;
-            }
-            else
-            {
-                n = Vector3.Empty;
-                return false;
-            }
-        }
-
-        private bool funesMori(TgcBoundingBox aabb) //TODO rename D:
-        {
             Vector3 boxDimensions = aabb.calculateSize() * 0.5f;
             Vector3 boxCenter = aabb.Position + boxDimensions;
 
-            Vector3 centerToCenter;
-            centerToCenter.X = FastMath.Abs(this.Center.X - boxCenter.X);
-            centerToCenter.Z = FastMath.Abs(this.Center.Z - boxCenter.Z);
+            Vector3 centerToCenter = new Vector3(this.Position.X - boxCenter.X, 0, this.Position.Z - boxCenter.Z);
+            Vector3 absCenterToCenter = new Vector3();
+            absCenterToCenter.X = FastMath.Abs(centerToCenter.X);
+            absCenterToCenter.Z = FastMath.Abs(centerToCenter.Z);
 
             //vemos si esta muy lejos
-            if (centerToCenter.X > (boxDimensions.X + this.radius)) return false;
-            if (centerToCenter.Z > (boxDimensions.Z + this.radius)) return false;
+            if (absCenterToCenter.X > (boxDimensions.X + this.radius)) return false;
+            if (absCenterToCenter.Z > (boxDimensions.Z + this.radius)) return false;
 
             //vemos si esta dentro del aabb
-            if (centerToCenter.X <= boxDimensions.X) return true;
-            if (centerToCenter.Z <= boxDimensions.Z) return true;
+            if (absCenterToCenter.X <= boxDimensions.X) goto celculateNormal;
+            if (absCenterToCenter.Z <= boxDimensions.Z) goto celculateNormal;
 
             //vemos si toca una esquina
-            float cornerDistance = FastMath.Pow2(centerToCenter.X - boxDimensions.X) + FastMath.Pow2(centerToCenter.Z - boxDimensions.Z);
-            return cornerDistance <= FastMath.Pow2(this.radius);
+            float cornerDistance = FastMath.Pow2(absCenterToCenter.X - boxDimensions.X) + FastMath.Pow2(absCenterToCenter.Z - boxDimensions.Z);
+            if (cornerDistance <= FastMath.Pow2(this.radius)) goto celculateNormal;
+
+            return false;
+
+        celculateNormal:
+            Vector2 boxDimensions2d = fromVector3(boxDimensions);
+            Vector2 centerToCenter2d = fromVector3(absCenterToCenter);
+
+            float cross = Vector2.Ccw(centerToCenter2d, boxDimensions2d);
+
+            if (cross > 0) n = new Vector3(centerToCenter.X, 0, 0);
+            else if (cross < 0) n = new Vector3(0, 0, centerToCenter.Z);
+            else n = new Vector3(centerToCenter.X * boxDimensions2d.Y, 0, centerToCenter.Z * boxDimensions2d.X);
+            n.Normalize();
+            return true;
+        }
+
+        private Vector2 fromVector3(Vector3 v3)
+        {
+            return new Vector2(v3.X, v3.Z);
         }
     }
 }
