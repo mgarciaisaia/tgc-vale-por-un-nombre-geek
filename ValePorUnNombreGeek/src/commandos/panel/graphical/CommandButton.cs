641b3c7be938858453861731ce7ec7e2a332e35f
pablo.santiago.fernandez@gmail.com <pablo.santiago.fernandez@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-05-11 23:52:08 +0000

    Empeze con los botones del panel. Primer problema: no se cual es el tamaño en pantalla de los botones. Uso Sprite.ImageInformation.Width * Sprite.Scale.X, pero parece que scale.x es 1

---
+++ ValePorUnNombreGeek/src/commandos/panel/graphical/CommandButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.panel.commands;
+using Microsoft.DirectX;
+
+namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.panel.graphical
+{
+    class CommandButton : Sprite
+    {
+        private Command command;
+
+        public CommandButton(Command _command, string _filePath)
+            : base(_filePath)
+        {
+            this.command = _command;
+        }
+
+        public void click()
+        {
+            this.command.execute();
+        }
+    }
+}
--- ValePorUnNombreGeek/src/commandos/panel/graphical/GraphicalControlPanel.cs
+++ ValePorUnNombreGeek/src/commandos/panel/graphical/GraphicalControlPanel.cs
@@ -6,6 +6,7 @@ using TgcViewer;
 using System.Drawing;
 using Microsoft.DirectX;
 using TgcViewer.Utils.Input;
+using AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.panel.commands;
 
 namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.panel.graphical
 {
@@ -13,22 +14,66 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.panel.graphical
     {
         private SpriteDrawer drawer;
         private Sprite controlPanelSprite;
+        private List<CommandButton> buttons;
 
         public GraphicalControlPanel()
         {
             int screenHeight = GuiController.Instance.D3dDevice.Viewport.Height;
             int screenWidth = GuiController.Instance.D3dDevice.Viewport.Width;
 
-            this.controlPanelSprite = new Sprite(GuiController.Instance.AlumnoEjemplosMediaDir + "ValePorUnNombreGeek\\Sprites\\panelMadera.png", GuiController.Instance.D3dDevice);
+            this.controlPanelSprite = new Sprite(GuiController.Instance.AlumnoEjemplosMediaDir + "ValePorUnNombreGeek\\Sprites\\panelMadera.png");
 
-            Rectangle drawingArea = new Rectangle(0, 0, this.controlPanelSprite.ImageInformation.Width, this.controlPanelSprite.ImageInformation.Height);
-            this.controlPanelSprite.RegionRectangle = drawingArea;
+            //Rectangle drawingArea = new Rectangle(0, 0, this.controlPanelSprite.ImageInformation.Width, this.controlPanelSprite.ImageInformation.Height);
+            //this.controlPanelSprite.RegionRectangle = drawingArea;
             this.controlPanelSprite.Position = new Vector2(0, screenHeight - this.controlPanelSprite.ImageInformation.Height);
             this.controlPanelSprite.Scale = new Vector2(screenWidth / (1.0f * this.controlPanelSprite.ImageInformation.Width), 1f);
-            
+
+            buttons = new List<CommandButton>();
 
             this.drawer = new SpriteDrawer();
             this.drawer.addSpriteToDraw(this.controlPanelSprite);
+        }
+
+        private void addButton(CommandButton _button)
+        {
+            this.buttons.Add(_button);
+            this.drawer.addSpriteToDraw(_button);
+
+            int X = this.controlPanelSprite.Width / 10;
+            foreach (CommandButton button in this.buttons)
+                X = Math.Max(X, (int)button.Position.X + button.Width + 5);
+
+            Vector2 pos = new Vector2(X, this.controlPanelSprite.Position.Y + this.controlPanelSprite.Height / 2 - _button.Height / 2);
+            _button.Position = pos;
+        }
+
+        public void addCommand(Command _command, string _picPath)
+        {
+            this.addButton(new CommandButton(_command, _picPath));
+        }
+
+        private bool mouseIsOverCommand(float mouseX, float mouseY, CommandButton command)
+        {
+            float spriteX = command.Position.X;
+            float spriteY = command.Position.Y;
+            float spriteWidth = command.Width;
+            float spriteHeight = command.Height;
+
+            return mouseX > spriteX && mouseX < spriteX + spriteWidth
+                && mouseY > spriteY && mouseY < spriteY + spriteHeight;
+        }
+
+        private bool commandUnderMouse(float mouseX, float mouseY, out CommandButton ret)
+        {
+            foreach (CommandButton button in this.buttons)
+                if (this.mouseIsOverCommand(mouseX, mouseY, button))
+                {
+                    ret = button;
+                    return true;
+                }
+
+            ret = null;
+            return false;
         }
 
         public void render()
@@ -54,18 +99,18 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.panel.graphical
         /// </summary>
         public void update()
         {
-            if(GuiController.Instance.D3dInput.buttonPressed(TgcViewer.Utils.Input.TgcD3dInput.MouseButtons.BUTTON_RIGHT)){
-                float relativeY = GuiController.Instance.D3dInput.Ypos - this.controlPanelSprite.Position.Y;
-                this.click(GuiController.Instance.D3dInput.Xpos, relativeY);
+            float mouseX = GuiController.Instance.D3dInput.Xpos;
+            float mouseY = GuiController.Instance.D3dInput.Ypos;
+            CommandButton button;
+            if (this.commandUnderMouse(mouseX, mouseY, out button))
+            {
+                if (GuiController.Instance.D3dInput.buttonPressed(TgcViewer.Utils.Input.TgcD3dInput.MouseButtons.BUTTON_LEFT))
+                    button.click();
+                else
+                {
+                    //TODO show tooltip
+                }
             }
-        }
-
-        /// <summary>
-        /// Metodo que se llama si hubo un click sobre el panel
-        /// </summary>
-        private void click(float panelx, float panely)
-        {
-            //TODO
         }
     }
 }
