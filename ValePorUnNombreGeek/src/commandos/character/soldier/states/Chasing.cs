db2e1bc6e58e89d38d7664b1ecf3d6bb90b7d528
dbkazarian@gmail.com <dbkazarian>
2013-05-11 01:50:50 +0000

    Faltaba un archivo

---
+++ ValePorUnNombreGeek/src/commandos/character/soldier/states/Chasing.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TgcViewer.Utils.Sound;
+using TgcViewer;
+
+namespace AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.character.soldier.states
+{
+    class Chasing:SoldierState
+    {
+        Commando commando;
+        TgcStaticSound talk;
+        public Chasing(Soldier _soldier, Commando commando):base(_soldier){
+            this.commando = commando;
+            talk = new TgcStaticSound();
+            talk.loadSound(GuiController.Instance.AlumnoEjemplosMediaDir + "ValePorUnNombreGeek//" + "Sounds//TALK.WAV");
+
+        }
+        public override void update(float elapsedTime)
+        {
+            if (this.soldier.canSee(commando))
+            {
+                if (GeneralMethods.isCloseTo(this.soldier.Position, commando.Position, this.soldier.Radius))
+                {
+                    this.soldier.Representation.talk();
+                    talk.play();
+                  
+                }
+                else
+                {
+
+                    this.soldier.setCharacterTarget(commando);
+                    this.soldier.goToTarget(elapsedTime);
+                    
+                }
+
+            }
+            else
+            {
+                this.soldier.setPreviousPositionTarget();
+                this.soldier.setState(new Waiting(soldier, 0));
+                
+            }
+        }
+    }
+}
