bb76264af8175529658667332a1bb2e18115355d
pablo.santiago.fernandez@gmail.com <pablo.santiago.fernandez@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-05-26 16:12:54 +0000

    Limpieza de la clase QuadTree. Al final si parece funcionar bien xd. Ahora voy a laburar en otro metodo que se me ocurrio anoche jaja.

--- trunk/ValePorUnNombreGeek/src/optimization/QuadTree.cs
+++ trunk/ValePorUnNombreGeek/src/optimization/QuadTree.cs
@@ -13,80 +13,90 @@ using TgcViewer;
 
 namespace AlumnoEjemplos.ValePorUnNombreGeek.src.optimization
 {
-    class QuadTree : AlumnoEjemplos.ValePorUnNombreGeek.src.optimization.IQuadTree
+    class QuadTree : IQuadTree
     {
-        List<ILevelObject> objects;
-        List<Character> characters;
-        List<QTSector> sectors;
+        private List<Character> characters;
+        private List<QTSector> sectors;
 
-        public IRenderer Renderer { get; set; }
 
+        /* QuadTree
+         * Optimiza el renderizado dibujando solo los sectores del terreno que colisionan
+         * con el frustum de la camara.
+         * Si hablamos de los objetos, cada uno esta relacionado a un sector, de manera
+         * que si un sector no se renderiza, sus objetos tampoco.
+         * Por el lado de los personajes, se busca la colision punto-frustum de su posicion
+         * para determinar si se renderiza o no.
+         * 
+         * Resultados en mi pc 26/05/2013:
+         * Sin optimizacion - 750fps
+         * QuadTree (grilla 3x3, viendo todo el mapa) - 700fps
+         * QuadTree (grilla 3x3, viendo de a 2 o 3 sectores) - entre 850 y 1000fps
+         */
+
+        #region Constructor
 
         public QuadTree(ITerrain terrain, IRenderer renderer)
         {
-            this.objects = new List<ILevelObject>();
-            this.characters = new List<Character>();
             this.Renderer = renderer;
+
+            this.characters = new List<Character>();
             sectors = new List<QTSector>();
+
             foreach(TerrainPatch tp in terrain.Patches)
                 sectors.Add(new QTSector(tp));
         }
 
         public void add(ILevelObject obstacle)
         {
-            this.objects.Add(obstacle);
             foreach (QTSector sector in this.sectors)
                 sector.addObjectIfCollides(obstacle);
-            //GuiController.Instance.UserVars.addVar("obj " + obstacle.GetHashCode().ToString());
         }
 
         public void add(Character ch)
         {
             this.characters.Add(ch);
-            //GuiController.Instance.UserVars.addVar("ch " + ch.GetHashCode().ToString());
         }
+
+        public IRenderer Renderer { get; set; }
+
+        #endregion
+
 
         public void render(TgcFrustum frustum)
         {
-            List<ILevelObject> objectsToRender = new List<ILevelObject>();
-
-            //El renderer se encarga de renderizarlos en el orden correcto y usar los shaders y pasadas correspondientes.
+            //iniciamos el renderer
             this.Renderer.beginRender();
-            //foreach (Character asd in this.characters)
-                //GuiController.Instance.UserVars.setValue("ch " + asd.GetHashCode().ToString(), false);
 
+
+            //buscamos los sectores del terreno que ve la camara
+            List<ILevelObject> objectsToRender = new List<ILevelObject>();
             foreach (QTSector sector in this.sectors)
                 if (sector.collidesWithFrustum(frustum))
                 {
                     objectsToRender.AddRange(sector.Objects);
                     this.Renderer.render(sector.TerrainPatch);
                 }
+
+
+            //buscamos los personajes que ve la camara
             foreach (Character ch in this.characters)
                 if (TgcCollisionUtils.testPointFrustum(frustum, ch.Position))
-                {
                     this.Renderer.render(ch);
-                    //GuiController.Instance.UserVars.setValue("ch " + ch.GetHashCode().ToString(), true);
-                }
 
-            //foreach (ILevelObject asd in this.objects)
-                //GuiController.Instance.UserVars.setValue("obj " + asd.GetHashCode().ToString(), false);
 
+            //mandamos a dibujar los objetos
             foreach (ILevelObject obj in objectsToRender)
-            {
                 this.Renderer.render(obj);
-                //GuiController.Instance.UserVars.setValue("obj " + obj.GetHashCode().ToString(), true);
-            }
 
+
+            //dibujamos
             this.Renderer.endRender();
-            
         }
+
 
         public void dispose()
         {
             this.Renderer.dispose();
-
         }
-
-
     }
 }
