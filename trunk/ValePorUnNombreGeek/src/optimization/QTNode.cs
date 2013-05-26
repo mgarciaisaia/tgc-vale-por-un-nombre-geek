1076e234ec27579583904958a5087a627067ac33
pablo.santiago.fernandez@gmail.com <pablo.santiago.fernandez@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-05-26 01:18:07 +0000

    Luego de toda una tarde luchando logre dividir el mapa en N sectores. Soy un queso, me costo una barbaridad. Como iba a saber que s epodian hacer funciones recursivas? Y para colmo al final fallaban los calculos por que al dividir dos enteros se retorna un entero. Maldito casteo.

---
+++ trunk/ValePorUnNombreGeek/src/optimization/QTNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AlumnoEjemplos.ValePorUnNombreGeek.src.commandos.objects;
+using TgcViewer.Utils.TgcGeometry;
+using Microsoft.DirectX;
+
+namespace AlumnoEjemplos.ValePorUnNombreGeek.src.optimization
+{
+    abstract class QTNode
+    {
+        private TgcBoundingBox aabb;
+
+        public void setBounds(Vector3 pmin, Vector3 pmax)
+        {
+            this.aabb = new TgcBoundingBox(pmin, pmax);
+        }
+
+        public bool seenByFrustum(TgcFrustum frustum)
+        {
+            TgcCollisionUtils.FrustumResult result = TgcCollisionUtils.classifyFrustumAABB(frustum, this.aabb);
+            return result != TgcCollisionUtils.FrustumResult.OUTSIDE;
+        }
+
+        public TgcBoundingBox BoundingBox { get { return this.aabb; } }
+
+        //abstract List<ILevelObject> getObjectsInside();
+    }
+}
