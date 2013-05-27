cb0b0f9299bd298dc15d4af1b8a8e39252f83a38
pablo.santiago.fernandez@gmail.com <pablo.santiago.fernandez@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-05-27 23:01:56 +0000

    Ahora el BackwardDiscard anda como corresponde, aunque no nos ahorra tantas fps =(. Tambien cambie la grilla de 3x3 a 4x4, en donde gano entre 100 y 200 fps mirando de media distancia, pero pierdo 100 si se mira todo el mapa. En fin, un balance bastante aceptable. Ahora me voy a poner a trabajar en un QuadTree que parta los sectores en mas sectores (como debe ser).

--- trunk/ValePorUnNombreGeek/src/optimization/BackwardDiscard.cs
+++ trunk/ValePorUnNombreGeek/src/optimization/BackwardDiscard.cs
@@ -25,48 +25,32 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.optimization
          * Sin optimizacion - 750fps
          * BackwardDiscard (grilla 3x3, viendo todo el mapa) - 750fps
          * BackwardDiscard (grilla 3x3, viendo de a 2 o 3 sectores) - entre 900 y 1100fps
-         * 
-         * Problemas a resolver:
-         * El dibujado de un sector del terreno tiene en cuenta su centro, por lo tanto cuando
-         * la camara sobrepasa este, no se renderiza el sector, quedando un agujero negro (xD).
          */
 
         protected override void filterAlgorithm(TgcFrustum frustum)
         {
             TgcCamera camera = GuiController.Instance.CurrentCamera;
+            //buscamos el plano de la camara
+            Vector3 n = camera.getLookAt() - camera.getPosition();
+            float d = Vector3.Dot(n, camera.getPosition());
 
-            //buscamos la direccion en la que mira la camara
-            Vector3 cameraSeen = camera.getLookAt() - camera.getPosition();
-            //hallamos su perpendicular
-            Vector3 cameraCut = Vector3.Cross(cameraSeen, new Vector3(0, 1, 0));
-
-
+            //filtramos todo lo que este detras de el
             foreach (Character ch in this.characters)
-            {
-                //ubicacion del personaje respecto de la camara
-                Vector3 chCameraPos = ch.Position - camera.getPosition();
-
-                //vemos si esta detras de la direccion en la que mira
-                Vector3 asd = Vector3.Cross(cameraCut, chCameraPos);
-                if (asd.Y > 0)
+                if (pointIsInFrontOfPlane(ch.Center, n, d))
                     this.filteredCharacters.Add(ch);
-            }
 
             foreach (TerrainPatch p in this.patches)
-            {
-                Vector3 tpCameraPos = p.BoundingBox.calculateBoxCenter() - camera.getPosition();
-                Vector3 asd = Vector3.Cross(cameraCut, tpCameraPos);
-                if (asd.Y > 0)
+                if (pointIsInFrontOfPlane(p.BoundingBox.calculateBoxCenter(), n, d))
                     this.filteredPatches.Add(p);
-            }
 
             foreach (ILevelObject o in this.objects)
-            {
-                Vector3 oCameraPos = o.Center - camera.getPosition();
-                Vector3 asd = Vector3.Cross(cameraCut, oCameraPos);
-                if (asd.Y > 0)
+                if (pointIsInFrontOfPlane(o.Center, n, d))
                     this.filteredObjects.Add(o);
-            }
+        }
+
+        private bool pointIsInFrontOfPlane(Vector3 point, Vector3 n, float d)
+        {
+            return Vector3.Dot(n, point) > d;
         }
     }
 }
