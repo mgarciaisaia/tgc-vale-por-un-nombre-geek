bf9dee630cb96423ad363daf3f4663eb117c0453
pablo.santiago.fernandez@gmail.com <pablo.santiago.fernandez@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-07-19 18:04:58 +0000

    LineStrip para el dibujado de las tapas del BoundingCylinder.

--- ValePorUnNombreGeek/src/cylinder/BoundingCylinder.cs
+++ ValePorUnNombreGeek/src/cylinder/BoundingCylinder.cs
@@ -86,7 +86,8 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.cylinder
         }
 
         private const int END_CAPS_RESOLUTION = 30; //cantidad de lineas por cada tapa
-        private CustomVertex.PositionColored[] endCapsVertex; //vertices de las tapas
+        private CustomVertex.PositionColored[] topCapVertex; //vertices de la tapa superior
+        private CustomVertex.PositionColored[] bottomCapVertex; //vertices de la tapa inferior
         private CustomVertex.PositionColored[] bordersVertex; //vertices de los bordes
 
         /// <summary>
@@ -94,28 +95,35 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.cylinder
         /// </summary>
         private void updateDraw()
         {
-            if (endCapsVertex == null)
+            int vertexCount = END_CAPS_RESOLUTION + 1; //el +1 es para cerrar las tapas
+            int color = this.borderColor;
+
+            if (this.topCapVertex == null)
             {
-                int verticesCount = (END_CAPS_RESOLUTION * 2 + 2) * 3;
-                verticesCount = verticesCount * 2; //por las dos tapas
-                this.endCapsVertex = new CustomVertex.PositionColored[verticesCount];
+                this.topCapVertex = new CustomVertex.PositionColored[vertexCount];
+                this.bottomCapVertex = new CustomVertex.PositionColored[vertexCount];
                 this.bordersVertex = new CustomVertex.PositionColored[4]; //bordes laterales
             }
 
-            float step = FastMath.TWO_PI / (float)END_CAPS_RESOLUTION;
-            int index = 0;
 
-            for (float a = 0f; a <= FastMath.TWO_PI; a += step)
+            Vector3 aPoint;
+            float angle;
+            float step = FastMath.TWO_PI / (float)(vertexCount - 1);
+
+            for (int index = 0; index < vertexCount - 1; index++)
             {
-                Vector3 tapaPuntoA = new Vector3(FastMath.Cos(a) * this.radius, 0, FastMath.Sin(a) * this.radius);
-                Vector3 tapaPuntoB = new Vector3(FastMath.Cos(a + step) * this.radius, 0, FastMath.Sin(a + step) * this.radius);
-                //tapa superior
-                endCapsVertex[index++] = new CustomVertex.PositionColored(tapaPuntoA + this.halfHeight + this.center, this.borderColor);
-                endCapsVertex[index++] = new CustomVertex.PositionColored(tapaPuntoB + this.halfHeight + this.center, this.borderColor);
-                //tapa inferior
-                endCapsVertex[index++] = new CustomVertex.PositionColored(tapaPuntoA - this.halfHeight + this.center, this.borderColor);
-                endCapsVertex[index++] = new CustomVertex.PositionColored(tapaPuntoB - this.halfHeight + this.center, this.borderColor);
+                    angle = step * index;
+                    aPoint = new Vector3(FastMath.Cos(angle) * this.radius, 0, FastMath.Sin(angle) * this.radius);
+                    
+                    this.topCapVertex[index] = new CustomVertex.PositionColored(aPoint + this.halfHeight + this.center, color);
+                    this.bottomCapVertex[index] = new CustomVertex.PositionColored(aPoint - this.halfHeight + this.center, color);
             }
+
+            //cerramos las tapas
+            this.topCapVertex[vertexCount - 1] = this.topCapVertex[0];
+            this.bottomCapVertex[vertexCount - 1] = this.bottomCapVertex[0];
+
+
             //bordes laterales
             this.updateBordersDraw();
         }
@@ -130,30 +138,34 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.cylinder
             transversalALaCamara.Normalize();
             transversalALaCamara *= this.radius;
 
-            Vector3 puntoA = this.center + this.halfHeight + transversalALaCamara;
-            Vector3 puntoB = this.center - this.halfHeight + transversalALaCamara;
+            int color = this.borderColor;
 
-            bordersVertex[0] = new CustomVertex.PositionColored(puntoA, this.borderColor);
-            bordersVertex[1] = new CustomVertex.PositionColored(puntoB, this.borderColor);
+            Vector3 pointA = this.center + this.halfHeight + transversalALaCamara;
+            Vector3 pointB = this.center - this.halfHeight + transversalALaCamara;
 
-            puntoA = this.center + this.halfHeight - transversalALaCamara;
-            puntoB = this.center - this.halfHeight - transversalALaCamara;
+            this.bordersVertex[0] = new CustomVertex.PositionColored(pointA, color);
+            this.bordersVertex[1] = new CustomVertex.PositionColored(pointB, color);
 
-            bordersVertex[2] = new CustomVertex.PositionColored(puntoA, this.borderColor);
-            bordersVertex[3] = new CustomVertex.PositionColored(puntoB, this.borderColor);
+            pointA = this.center + this.halfHeight - transversalALaCamara;
+            pointB = this.center - this.halfHeight - transversalALaCamara;
+
+            this.bordersVertex[2] = new CustomVertex.PositionColored(pointA, color);
+            this.bordersVertex[3] = new CustomVertex.PositionColored(pointB, color);
         }
 
         public void render()
         {
             Device d3dDevice = GuiController.Instance.D3dDevice;
             this.updateBordersDraw();
-            d3dDevice.DrawUserPrimitives(PrimitiveType.LineList, endCapsVertex.Length / 2, endCapsVertex);
+            d3dDevice.DrawUserPrimitives(PrimitiveType.LineStrip, this.topCapVertex.Length - 1, this.topCapVertex);
+            d3dDevice.DrawUserPrimitives(PrimitiveType.LineStrip, this.bottomCapVertex.Length - 1, this.bottomCapVertex);
             d3dDevice.DrawUserPrimitives(PrimitiveType.LineList, bordersVertex.Length / 2, bordersVertex);
         }
 
         public void dispose()
         {
-            this.endCapsVertex = null;
+            this.topCapVertex = null;
+            this.bottomCapVertex = null;
             this.bordersVertex = null;
         }
 
