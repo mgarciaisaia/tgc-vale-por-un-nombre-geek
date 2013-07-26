8cda9c251e40dc5386908e2dafaeeaa8a304a050
pablo.santiago.fernandez@gmail.com <pablo.santiago.fernandez@gmail.com@d1f6c574-5d87-3def-a24b-78f3f2897a11>
2013-07-26 00:42:10 +0000

    Texture mapping en la cara lateral del cilindro.

--- ValePorUnNombreGeek/src/cylinder/Cylinder.cs
+++ ValePorUnNombreGeek/src/cylinder/Cylinder.cs
@@ -8,6 +8,7 @@ using Microsoft.DirectX.Direct3D;
 using TgcViewer.Utils.TgcGeometry;
 using TgcViewer;
 using System.Drawing;
+using TgcViewer.Utils.Shaders;
 
 namespace AlumnoEjemplos.ValePorUnNombreGeek.src.cylinder
 {
@@ -19,8 +20,13 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.cylinder
         private BoundingCylinder boundingCylinder;
 
         private const int END_CAPS_RESOLUTION = 40;
-        private CustomVertex.PositionColored[] sideTrianglesVertices; //triangle strip
-        private CustomVertex.PositionColored[] capsTrianglesVertices; //triangle list
+        private CustomVertex.PositionColoredTextured[] sideTrianglesVertices; //triangle strip
+        private CustomVertex.PositionColoredTextured[] capsTrianglesVertices; //triangle list
+
+        private bool useTexture;
+        private TgcTexture texture;
+        private Effect effect;
+        private string technique;
 
 
         public Cylinder(Vector3 _center, float _radius, float _halfLength)
@@ -29,8 +35,7 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.cylinder
             this.halfLength = _halfLength;
             this.boundingCylinder = new BoundingCylinder(_center, _radius, _halfLength);
 
-            //this.color = Color.Red.ToArgb();
-            this.color = Color.FromArgb(150, Color.Red).ToArgb();
+            this.color = Color.Red.ToArgb();
 
             this.initialize();
         }
@@ -40,12 +45,26 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.cylinder
             int capsResolution = END_CAPS_RESOLUTION;
 
             //cara lateral: un vertice por cada vertice de cada tapa, mas dos para cerrarla
-            this.sideTrianglesVertices = new CustomVertex.PositionColored[2 * capsResolution + 2];
+            this.sideTrianglesVertices = new CustomVertex.PositionColoredTextured[2 * capsResolution + 2];
 
             //tapas: dos vertices por cada vertice de cada tapa, mas uno en el centro
-            this.capsTrianglesVertices = new CustomVertex.PositionColored[capsResolution * 3 * 2];
+            this.capsTrianglesVertices = new CustomVertex.PositionColoredTextured[capsResolution * 3 * 2];
 
+            this.useColorShader();
             this.updateValues();
+        }
+
+        private void useColorShader()
+        {
+            this.effect = GuiController.Instance.Shaders.VariosShader;
+            this.technique = TgcShaders.T_POSITION_COLORED;
+            this.useTexture = false;
+        }
+
+        private void useTextureShader()
+        {
+            this.technique = TgcShaders.T_POSITION_COLORED_TEXTURED;
+            this.useTexture = true;
         }
 
         private void updateDraw()
@@ -58,7 +77,8 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.cylinder
 
             //matriz de rotacion del vector de dibujado
             float angleStep = FastMath.TWO_PI / (float)capsResolution;
-            Matrix rotationMatrix = Matrix.RotationAxis(upVector, angleStep);
+            Matrix rotationMatrix = Matrix.RotationAxis(-upVector, angleStep);
+            float angle = 0;
 
             //transformacion que se le aplicara a cada vertice
             Matrix transformation = this.Transform;
@@ -67,47 +87,55 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.cylinder
             Vector3[] topCapDraw = new Vector3[capsResolution];
             Vector3[] bottomCapDraw = new Vector3[capsResolution];
 
-            Vector3 topCapCenter = Vector3.TransformCoordinate(upVector, transformation);
-            Vector3 bottomCapCenter = Vector3.TransformCoordinate(-upVector, transformation);
+            //Vector3 topCapCenter = Vector3.TransformCoordinate(upVector, transformation);
+            //Vector3 bottomCapCenter = Vector3.TransformCoordinate(-upVector, transformation);
+            Vector3 topCapCenter = upVector;
+            Vector3 bottomCapCenter = -upVector;
 
             for (int i = 0; i < capsResolution; i++)
             {
                 //establecemos los vertices de las tapas
-                topCapDraw[i] = Vector3.TransformCoordinate(upVector + n, transformation);
-                bottomCapDraw[i] = Vector3.TransformCoordinate(-upVector + n, transformation);
+                //topCapDraw[i] = Vector3.TransformCoordinate(upVector + n, transformation);
+                //bottomCapDraw[i] = Vector3.TransformCoordinate(-upVector + n, transformation);
+                topCapDraw[i] = upVector + n;
+                bottomCapDraw[i] = -upVector + n;
+
+                float u = angle / FastMath.TWO_PI;
 
                 //triangulos de la cara lateral (strip)
-                this.sideTrianglesVertices[2 * i] = new CustomVertex.PositionColored(topCapDraw[i], color);
-                this.sideTrianglesVertices[2 * i + 1] = new CustomVertex.PositionColored(bottomCapDraw[i], color);
+                this.sideTrianglesVertices[2 * i] = new CustomVertex.PositionColoredTextured(topCapDraw[i], color, u, 0);
+                this.sideTrianglesVertices[2 * i + 1] = new CustomVertex.PositionColoredTextured(bottomCapDraw[i], color, u, 1);
 
                 //triangulos de la tapa superior (list)
-                if (i > 0) this.capsTrianglesVertices[3 * i] = new CustomVertex.PositionColored(topCapDraw[i - 1], color);
-                this.capsTrianglesVertices[3 * i + 1] = new CustomVertex.PositionColored(topCapDraw[i], color);
-                this.capsTrianglesVertices[3 * i + 2] = new CustomVertex.PositionColored(topCapCenter, Color.White.ToArgb());
+                if (i > 0) this.capsTrianglesVertices[3 * i] = new CustomVertex.PositionColoredTextured(topCapDraw[i - 1], color, FastMath.Cos(angle - angleStep), FastMath.Sin(angle - angleStep));
+                this.capsTrianglesVertices[3 * i + 1] = new CustomVertex.PositionColoredTextured(topCapDraw[i], color, FastMath.Cos(angle), FastMath.Sin(angle));
+                this.capsTrianglesVertices[3 * i + 2] = new CustomVertex.PositionColoredTextured(topCapCenter, color, 0.5f, 0.5f);
 
                 //triangulos de la tapa inferior (list)
-                if (i > 0) this.capsTrianglesVertices[3 * i + 3 * capsResolution] = new CustomVertex.PositionColored(bottomCapDraw[i - 1], color);
-                this.capsTrianglesVertices[3 * i + 1 + 3 * capsResolution] = new CustomVertex.PositionColored(bottomCapDraw[i], color); ;
-                this.capsTrianglesVertices[3 * i + 2 + 3 * capsResolution] = new CustomVertex.PositionColored(bottomCapCenter, Color.White.ToArgb());
+                if (i > 0) this.capsTrianglesVertices[3 * i + 3 * capsResolution] = new CustomVertex.PositionColoredTextured(bottomCapDraw[i - 1], color, FastMath.Cos(angle - angleStep), FastMath.Sin(angle - angleStep));
+                this.capsTrianglesVertices[3 * i + 1 + 3 * capsResolution] = new CustomVertex.PositionColoredTextured(bottomCapDraw[i], color, FastMath.Cos(angle), FastMath.Sin(angle)); ;
+                this.capsTrianglesVertices[3 * i + 2 + 3 * capsResolution] = new CustomVertex.PositionColoredTextured(bottomCapCenter, color, 0.5f, 0.5f);
 
                 //rotamos el vector de dibujado
                 n.TransformNormal(rotationMatrix);
+                angle += angleStep;
             }
 
             //cerramos la cara lateral
-            this.sideTrianglesVertices[2 * capsResolution] = new CustomVertex.PositionColored(topCapDraw[0], color);
-            this.sideTrianglesVertices[2 * capsResolution + 1] = new CustomVertex.PositionColored(bottomCapDraw[0], color);
+            this.sideTrianglesVertices[2 * capsResolution] = new CustomVertex.PositionColoredTextured(topCapDraw[0], color, 1, 0);
+            this.sideTrianglesVertices[2 * capsResolution + 1] = new CustomVertex.PositionColoredTextured(bottomCapDraw[0], color, 1, 1);
 
             //cerramos la tapa superior
-            this.capsTrianglesVertices[0] = new CustomVertex.PositionColored(topCapDraw[capsResolution - 1], color);
+            this.capsTrianglesVertices[0] = new CustomVertex.PositionColoredTextured(topCapDraw[capsResolution - 1], color, FastMath.Cos(-angleStep), FastMath.Sin(-angleStep));
 
             //Cerramos la tapa inferior
-            this.capsTrianglesVertices[3 * capsResolution] = new CustomVertex.PositionColored(bottomCapDraw[capsResolution - 1], color);
+            this.capsTrianglesVertices[3 * capsResolution] = new CustomVertex.PositionColoredTextured(bottomCapDraw[capsResolution - 1], color, FastMath.Cos(-angleStep), FastMath.Sin(-angleStep));
         }
 
         public void render()
         {
             Device d3dDevice = GuiController.Instance.D3dDevice;
+            TgcTexture.Manager texturesManager = GuiController.Instance.TexturesManager;
 
             if (this.AlphaBlendEnable)
             {
@@ -115,20 +143,42 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.cylinder
                 d3dDevice.RenderState.AlphaTestEnable = true;
             }
 
+            if (texture != null)
+                texturesManager.shaderSet(effect, "texDiffuseMap", texture);
+            else
+                texturesManager.clear(0);
+            texturesManager.clear(1);
+
+            GuiController.Instance.Shaders.setShaderMatrix(this.effect, this.Transform);
+            d3dDevice.VertexDeclaration = GuiController.Instance.Shaders.VdecPositionColoredTextured;
+            effect.Technique = this.technique;
+
             int capsResolution = END_CAPS_RESOLUTION;
 
-            //FillMode old = d3dDevice.RenderState.FillMode;
-            //d3dDevice.RenderState.FillMode = FillMode.WireFrame;
+            effect.Begin(0);
+            effect.BeginPass(0);
             d3dDevice.DrawUserPrimitives(PrimitiveType.TriangleStrip, 2 * capsResolution, this.sideTrianglesVertices);
             d3dDevice.DrawUserPrimitives(PrimitiveType.TriangleList, 2 * capsResolution, this.capsTrianglesVertices);
-            //d3dDevice.RenderState.FillMode = old;
+            effect.EndPass();
+            effect.End();
 
             d3dDevice.RenderState.AlphaTestEnable = false;
             d3dDevice.RenderState.AlphaBlendEnable = false;
+
+
+            //int capsResolution = END_CAPS_RESOLUTION;
+
+            ////FillMode old = d3dDevice.RenderState.FillMode;
+            ////d3dDevice.RenderState.FillMode = FillMode.WireFrame;
+            //
+            //
+            ////d3dDevice.RenderState.FillMode = old;
+
         }
 
         public void dispose()
         {
+            if (this.texture != null) this.texture.dispose();
             this.sideTrianglesVertices = null;
             this.capsTrianglesVertices = null;
             this.boundingCylinder.dispose();
@@ -208,6 +258,49 @@ namespace AlumnoEjemplos.ValePorUnNombreGeek.src.cylinder
         }
 
         #endregion
+
+        /// <summary>
+        /// Shader del mesh
+        /// </summary>
+        public Effect Effect
+        {
+            get { return this.effect; }
+            set { this.effect = value; }
+        }
+
+        /// <summary>
+        /// Technique que se va a utilizar en el effect.
+        /// Cada vez que se llama a render() se carga este Technique (pisando lo que el shader ya tenia seteado)
+        /// </summary>
+        public string Technique
+        {
+            get { return this.technique; }
+            set { this.technique = value; }
+        }
+
+        /// <summary>
+        /// Setea la textura
+        /// </summary>
+        public void setTexture(TgcTexture _texture)
+        {
+            if (this.texture != null)
+                this.texture.dispose();
+            this.texture = _texture;
+        }
+
+        /// <summary>
+        /// Habilita el dibujado de la textura
+        /// </summary>
+        public bool UseTexture {
+            get { return this.useTexture; }
+            set
+            {
+                if (value)
+                    this.useTextureShader();
+                else
+                    this.useColorShader();
+            }
+        }
 
         public BoundingCylinder BoundingCylinder { get { return this.boundingCylinder; } }
 
